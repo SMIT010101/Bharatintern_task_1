1c80d25b48fe440c3d4b035499f80d68ae15400f
SMIT010101 <104785883+SMIT010101@users.noreply.github.com>
2023-09-17 17:57:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/splasj.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splasj extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity
+                Intent intent = new Intent(splasj.this, MainActivity.class);
+                startActivity(intent);
+
+                // Finish the splash activity
+                finish();
+            }
+        }, 2000);
+    }
+}
